b7a45999fc50dfd51a88bd975251a6b4613bc299
nm86nm <nm86nm@gmail.com>
2025-02-06 18:05:57 +0100

    Initial commit

---
+++ app/src/main/java/com/mnp/interfaceandnavigation/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.mnp.interfaceandnavigation
+
+import android.os.Bundle
+import android.preference.EditTextPreference
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.mnp.interfaceandnavigation.ui.theme.InterfaceAndNavigationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Navigation()
+        }
+    }
+}
