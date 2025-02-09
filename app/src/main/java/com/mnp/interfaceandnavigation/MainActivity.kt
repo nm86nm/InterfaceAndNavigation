5c1e47c0b552db9cecbe4485debb891ebbcd499b
nm86nm <nm86nm@gmail.com>
2025-02-09 18:41:31 +0100

    Project Improvement

--- app/src/main/java/com/mnp/interfaceandnavigation/MainActivity.kt
+++ app/src/main/java/com/mnp/interfaceandnavigation/MainActivity.kt
@@ -6,11 +6,14 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.mnp.interfaceandnavigation.ui.theme.InterfaceAndNavigationTheme
@@ -19,7 +22,13 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Navigation()
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ){
+                Navigation()
+            }
         }
     }
 }
